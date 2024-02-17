a30b204d4c442a039889aaea27bf1acccf6ff906
ahmedl3swy <ahmed.a.alesawy@gmail.com>
2024-02-17 14:24:46 +0200

    Attendance Record Class

---
+++ XmlDataManger/AttendanceRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlDataManger
+{
+    public class AttendanceRecord
+    {
+        public string StudentID { get; set; }
+        public string Date { get; set; }
+        public string Status { get; set; }
+
+        public AttendanceRecord(string studentID, string date, string status)
+        {
+            StudentID = studentID;
+            Date = date;
+            Status = status;
+        }
+
+        public override string ToString()
+        {
+            return $"StudentID: {StudentID}, Date: {Date}, Status: {Status}";
+        }
+    }
+}
