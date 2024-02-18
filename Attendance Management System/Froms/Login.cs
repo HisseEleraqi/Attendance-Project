db30f9a225074b2940afcfe859cf0f1aa04aaaf5
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-02-18 19:04:21 +0200

    Added initial structure

---
+++ Attendance Management System/Froms/Login.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Attendance_Management_System.Froms
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form teacherForm = new Teacher
+            {
+                userName = "Teacher1",
+                userRole = "Teacher"
+            };
+            teacherForm.Show();
+        }
+    }
+}
